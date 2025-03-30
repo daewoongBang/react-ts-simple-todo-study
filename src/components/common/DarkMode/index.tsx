738fabc907101716d844365d43d2d478c801fec1
Daewoong Bang <59325523+daewoongBang@users.noreply.github.com>
2025-03-30 23:39:14 +0900

    style: apply dark mode styles

--- src/components/common/DarkMode/index.tsx
+++ src/components/common/DarkMode/index.tsx
@@ -1,16 +1,15 @@
 import { HiMoon, HiSun } from 'react-icons/hi';
 
 import { useDarkMode } from 'context/DarkModeContext';
+import styles from './styles.module.css';
 
 const DarkMode = () => {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
 
   return (
-    <div>
-      <button type='button' onClick={toggleDarkMode}>
-        {isDarkMode ? <HiSun /> : <HiMoon />}
-      </button>
-    </div>
+    <button className={styles.button} type='button' onClick={toggleDarkMode}>
+      {isDarkMode ? <HiSun /> : <HiMoon />}
+    </button>
   );
 };
 
